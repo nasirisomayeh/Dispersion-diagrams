ed81b8e921d3010168b848ec813154fd6c07a763
somaye <nasirisomayeh2@gmail.com>
2022-02-02 22:20:34 +0330

    Dispersion_diagrams

---
+++ Dispersion_diagrams.py
@@ -0,0 +1,10 @@
+from turtle import color
+import matplotlib.pyplot as nem
+
+x = [1, 3, 5, 7, 9, 11]
+y = [2, 5, 7, 8, 0, 7]
+
+nem.scatter(x,y,color="red", 
+            marker="*", s=300)
+
+nem.show()
